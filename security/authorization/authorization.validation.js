1905a26953e07dd7ee6a67ef4bca2b73ed4aef29
Yahia <yahiafellah@yahoo.fr>
2020-01-22 12:26:00 +0100

    fix: refreshToken Verification

--- security/authorization/authorization.validation.js
+++ security/authorization/authorization.validation.js
@@ -6,15 +6,14 @@ fs = require('fs');
 const cert = fs.readFileSync('./tls/token-public-key.pem');
 
 exports.validJWTNeeded = (req, res, next) => {
-    console.log('hello')
     if (req.headers['authorization']) {
         try {
             let authorization = req.headers['authorization'].split(' ');
             if (authorization[0] !== 'Bearer') {
                 return res.status(401).send();
             } else {
-                var aud = 'urn:' + (req.get('origin') ? req.get('origin') : "kaaniche.xyz");
-                req.jwt = jwt.verify(authorization[1], cert, { issuer: "urn:kaaniche.xyz", audience: aud, algorithms: ['RS512'] });
+                var aud = 'urn:' + (req.get('origin') ? req.get('origin') : "yahia.xyz");
+                req.jwt = jwt.verify(authorization[1], cert, { issuer: "urn:yahia.xyz", audience: aud, algorithms: ['RS512'] });
                 return next();
             }
         } catch (err) {
@@ -34,14 +33,11 @@ exports.verifyRefreshBodyField = (req, res, next) => {
 };
 
 exports.validRefreshNeeded = (req, res, next) => {
-    let b = Buffer.from(req.body.refresh_token, 'base64');
-    let decoded = b.toString().split('$');
+    let decoded = req.body.refresh_token.split('$');
     let salt = decoded[0];
 
     let refresh_token = decoded[1];
-    let hash = crypto.createHmac('sha512', salt).update(req.jwt.user_id + refreshSecret + req.jwt.jti).digest("base64");
-    // req.body = req.jwt;
-    // return next();
+    let hash = crypto.createHmac('sha512', salt).update(req.jwt.userId + refreshSecret + req.jwt.jti).digest("base64");
     if (hash === refresh_token) {
         req.body = req.jwt;
         return next();
